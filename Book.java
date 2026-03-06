8a19c36fb75c9f71bbbba589c7a5c1df4f602a86
abirami2322005-abcd <abirami2322005@gmail.com>
2026-03-06 20:03:56 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Book{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int copies=scan.nextInt();
+        int cost=scan.nextInt();
+        int editing=scan.nextInt();
+        int design=scan.nextInt();
+        int commission=scan.nextInt();
+        int marketing=scan.nextInt();
+        int publishingprofit=(copies*cost)+editing
++design-commission-marketing;        
+        System.out.println(publishingprofit);
+    }
+}
