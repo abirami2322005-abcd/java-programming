8f69e6ed218304e6f82a191db6586b5e6ae88294
abirami2322005-abcd <abirami2322005@gmail.com>
2026-03-08 18:28:04 +0530

    Add files via upload

---
+++ Online.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Online{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+
+int theory=scan.nextInt();
+int practical=scan.nextInt();
+
+double avg=(theory+practical)/2.0;
+
+if(theory>=50 && practical>=50 && avg>=60)
+System.out.println("Result = PASS");
+else
+System.out.println("Result = FAIL");
+}
+}
