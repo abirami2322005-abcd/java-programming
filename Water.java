2c89f399f1f1fb5f0cfdcee9f2cf6ae806a8a634
abirami2322005-abcd <abirami2322005@gmail.com>
2026-03-08 17:49:32 +0530

    Add files via upload

---
+++ Water.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Water {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int resusaget=scan.nextInt();
+        int resrate=scan.nextInt();
+        int comusage=scan.nextInt();
+        int comrate=scan.nextInt();
+        int connection=scan.nextInt();
+        int penality=scan.nextInt();
+        int maintenance=scan.nextInt();
+        int treatment=scan.nextInt();
+        int netwaterrevenue=(resusaget*resrate)+(comusage*comrate)+connection+penality-maintenance-treatment;
+        System.out.println(netwaterrevenue);
+    }
+}
