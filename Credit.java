159a8bf7217624cbcccddd17518b4dd6be136453
abirami2322005-abcd <abirami2322005@gmail.com>
2026-03-08 18:29:30 +0530

    Add files via upload

---
+++ Credit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Credit{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+
+int limit=scan.nextInt();
+int purchase=scan.nextInt();
+int blocked=scan.nextInt();
+
+if(purchase<=limit && blocked==0)
+System.out.println("Approved");
+else
+System.out.println("Declined");
+}
+}
