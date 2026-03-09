9af91d089750d2caec456855e46fa6ae877fd188
abirami2322005-abcd <abirami2322005@gmail.com>
2026-03-09 12:15:53 +0530

    Add files via upload

---
+++ Ifelse8.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ifelse8 {
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int units=sc.nextInt();
+    int fluctuation=sc.nextInt();
+    if(units>500 || fluctuation==1)
+    System.out.println("Alert Triggered");
+    else
+    System.out.println("Normal Usage");
+    }
+}
