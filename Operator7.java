232abd7616fcc2d4998753a1f9af5359091301a5
abirami2322005-abcd <abirami2322005@gmail.com>
2026-03-15 19:26:51 +0530

    Add files via upload

---
+++ Operator7.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Operator7{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int roomcharge=scan.nextInt();
+        int days=scan.nextInt();
+        int medicinecost=scan.nextInt();
+        int labfees=scan.nextInt();
+        int insurancecoverage=scan.nextInt();
+        int payableamount=(roomcharge*days)+medicinecost+labfees-insurancecoverage;
+        System.out.println("Payableamount="+payableamount);
+   }
+}
