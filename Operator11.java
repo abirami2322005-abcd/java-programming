0091a4907141e5eddee97ccf823535bb46865771
abirami2322005-abcd <abirami2322005@gmail.com>
2026-03-16 20:02:10 +0530

    Add files via upload

---
+++ Operator11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Operator11 {
+    
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int ticketrevenue=scan.nextInt();
+        int snackrevenue=scan.nextInt();
+        int maintenance=scan.nextInt();
+        int electricity=scan.nextInt();
+        int netprofit=ticketrevenue+snackrevenue-maintenance-electricity;
+        System.out.println(netprofit);
+    }
+}
