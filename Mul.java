ede194def04e8f76118590257d7568b660016b98
abirami2322005-abcd <abirami2322005@gmail.com>
2026-04-04 09:58:46 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Mul {
+    public static void main(String[] args) {
+        Scanner object=new Scanner(System.in);
+        int a=object.nextInt();
+        int b=object.nextInt();
+        int c=a*b;
+        System.out.println(c);
+        object.close();
+    }
+}
