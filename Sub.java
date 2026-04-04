4f165bd7db05f718248858e280ec2487684d2df6
abirami2322005-abcd <abirami2322005@gmail.com>
2026-04-04 09:58:17 +0530

    Add files via upload

---
+++ Sub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sub{
+    public static void main(String[] args) {
+        Scanner object=new Scanner(System.in);
+        int a=object.nextInt();
+        int b=object.nextInt();
+        int c=a-b;
+        System.out.println(c);
+        object.close();
+    }
+} 
+    
+
