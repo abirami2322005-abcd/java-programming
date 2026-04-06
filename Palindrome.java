6703b0095fbc4dc366344a1b466447ec4b1e2a90
abirami2322005-abcd <abirami2322005@gmail.com>
2026-04-06 09:12:51 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, original = num, reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reversed) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
