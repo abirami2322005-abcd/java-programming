914479e244fc6c634e4957c2267681ad3169726b
abirami2322005-abcd <abirami2322005@gmail.com>
2026-04-11 09:38:24 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int sum = a + b + c;
+        System.out.println("Sum = " + sum);
+    }
+}
